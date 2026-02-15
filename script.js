fd15e6efc0275d54a3229cf5eb8d39fd65109c97
VladislavAmbrosov <ambrosov413@gmail.com>
2026-02-15 12:53:05 +0300

    init project with all files

---
+++ script.js
@@ -0,0 +1,19 @@
+const model = {
+
+}
+
+const view = {
+    init(){
+
+    }
+}
+
+const controler = {
+
+}
+
+function init() {
+    view.init()
+}
+
+document.addEventListener('DOMContentLoaded', init)
